3e514faaa0d99a90c7f5f6092d6f0137cf74c2a6
lethabo kekana <lethaboedith@gmail.com>
2022-12-02 20:40:03 +0200

    first commit

---
+++ chat-bot-admin-master/.eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:vue/base",
+    "@vue/eslint-config-prettier",
+    "plugin:vue/vue3-essential",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
---
+++ chat-bot-admin-master/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import TopMenuBar from "./components/TopMenuBar.vue";
+import SideMenuBar from "./components/SideMenuBar.vue";
+</script>
+
+<template>
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <side-menu-bar></side-menu-bar>
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <!-- Topbar -->
+        <top-menu-bar></top-menu-bar>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          <RouterView />
+        </div>
+        <!-- /.container-fluid -->
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Thabo Chat Bot 2022</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+    </div>
+    <!-- End of Content Wrapper -->
+  </div>
+</template>
+
+<style scoped></style>
